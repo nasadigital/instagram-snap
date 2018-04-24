9e27bee1002962815b221ad2796eec22e0cc515b
nasadigital <Uchia_Madara@yahoo.com>
2018-04-24 12:34:38 +0200

    Check friendship property

--- examples/testgraph/testgraph.cpp
+++ examples/testgraph/testgraph.cpp
@@ -4,6 +4,24 @@
 
 typedef unsigned int uint;
 
+double get_average(std::vector<int> ar) {
+  double sum = 0;
+  for (auto it : ar)
+    sum += it;
+  return sum / ar.size();
+}
+
+double get_median(std::vector<int> ar) {
+  if (ar.size() == 1)
+    return ar[0];
+  nth_element(ar.begin(), ar.begin() + ar.size() / 2, ar.end());
+  double rez = ar[ar.size() / 2];
+  if (!(ar.size() & 1)) {
+    nth_element(ar.begin(), ar.begin() + ar.size() / 2 - 1, ar.end());
+    rez = (rez + ar[ar.size() / 2 - 1]) / 2;
+  }
+  return rez;
+}
 
 std::vector<std::string> split(std::string &s, char delim) {
   std::vector<std::string> rez;
@@ -45,7 +63,7 @@ int main(int argc, char* argv[]) {
   std::cout << "Done Calculating Statistics!\n";
 
   std::cout << "Start Checking Friendship Properties...\n";
-  int worse_than_friends = 0;
+  int friends_weak = 0, friends_strong = 0, total_with_friends = 0;
   std::vector<int> total_likes[GRAPH_SIZE];
   for (auto it = instagram_network->BegNI();
        it < instagram_network->EndNI(); it++) {
@@ -54,6 +72,30 @@ int main(int argc, char* argv[]) {
           instagram_network->GetEDat(it.GetId(), it.GetOutNId(e)));
     }
   } 
+
+  for (auto it = instagram_network->BegNI();
+       it < instagram_network->EndNI(); it++) {
+    std::vector<int> friend_likes;
+    for (int e = 0; e < it.GetOutDeg(); e++) {
+      friend_likes.push_back(
+          accumulate(total_likes[it.GetOutNId(e)].begin(),
+                     total_likes[it.GetOutNId(e)].end(), 0));
+    }
+    if (friend_likes.size()) {
+      ++total_with_friends;
+      if (friend_likes[it.GetId()] < get_average(friend_likes))
+        ++friends_weak;
+      if (friend_likes[it.GetId()] < get_median(friend_likes))
+        ++friends_strong;
+    }
+  }
+  std::cout << "Friendship paradox (weak) : " << friends_weak << " / "
+            << total_with_friends << " - "
+            << (100.0 * friends_weak) / total_with_friends << "%\n";
+  std::cout << "Friendship paradox (strong) : " << friends_strong << " / "
+            << total_with_friends << " - "
+            << (100.0 * friends_strong) / total_with_friends << "%\n";
+
   std::cout << "Done Checking Friendship Properties!\n";
   //// what type of graph do you want to use?
   typedef PUNGraph PGraph; // undirected graph
