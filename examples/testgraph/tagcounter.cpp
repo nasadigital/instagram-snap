769a6401c427fd7817c65313936bb469b6946009
nasadigital <Uchia_Madara@yahoo.com>
2018-05-08 14:24:26 +0200

    Refactor TagCounter class

--- examples/testgraph/tagcounter.cpp
+++ examples/testgraph/tagcounter.cpp
@@ -30,21 +30,18 @@ void TagCounter::export_occurances(std::string filename) {
   fout.close();
 }
 
-std::vector<std::pair<long long, int>>
-    UniqueTagCounter::get_timeline(std::string tag_name) {
-  if (!tag_timeline.count(tag_name)) {
-    std::cout << "No tag called " << tag_name << " found!\n";
-  }
-  std::unordered_map<int, int> first_occ;
-  std::vector<std::pair<long long, int>> rez;
-  for (auto it : tag_timeline[tag_name]) {
-    if (first_occ.count(it.second)) {
-      rez[first_occ[it.second]].first =
-          std::min(rez[first_occ[it.second]].first, it.first);
+void UniqueTagCounter::process_line(std::vector<std::string> split_line) {
+  if (split_line.size() < 7)
+    return;
+  int id = stoi(split_line[1]);
+  long long ts = stoll(split_line[2]);
+  for (auto tag : split(split_line[3], ',')) {
+    if (first_occ[tag].count(id)) {
+       tag_timeline[tag][first_occ[tag][id]].first =
+         std::min(tag_timeline[tag][first_occ[tag][id]].first, ts);
     } else {
-      first_occ[it.second] = rez.size();
-      rez.push_back(it);
+      first_occ[tag][id] = tag_timeline[tag].size();
+      tag_timeline[tag].push_back(std::make_pair(ts, id));
     }
   }
-  return rez;
 }
--- examples/testgraph/tagcounter.h
+++ examples/testgraph/tagcounter.h
@@ -5,12 +5,15 @@ class TagCounter {
   std::unordered_map<std::string,
                      std::vector<std::pair<long long, int>>> tag_timeline; 
  public:
-  void process_line(std::vector<std::string> split_line);
-  virtual std::vector<std::pair<long long, int>> get_timeline(
+  virtual void process_line(std::vector<std::string> split_line);
+  std::vector<std::pair<long long, int>> get_timeline(
       std::string tag_name);
   void export_occurances(std::string filename);
 };
 
 class UniqueTagCounter : public TagCounter {
-  std::vector<std::pair<long long, int>> get_timeline(std::string tag_name);
+  std::unordered_map<std::string, std::unordered_map<int, int>> first_occ;
+
+ public:
+  void process_line(std::vector<std::string> split_line);
 };
